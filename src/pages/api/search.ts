fe16af13efa2d44467e004a68d87e86f046788e2
Troyer, Ronald <ronald.troyer@chase.com>
2021-08-04 09:12:59 -0400

    Should catch the block, not the await

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -1,21 +1,20 @@
 import * as StitcherAPI from 'src/server/lib/StitcherAPI';
 import { NextApiHandler } from 'next';
 import { ApiError } from 'next/dist/next-server/server/api-utils';
-
 const api: NextApiHandler = async (req, res) => {
-  const { query }: { query?: string } = req.query;
-  if (query?.length) {
-    const premiumShows = await StitcherAPI.getSearchPremiumShows(query).catch((error) => {
-      return new ApiError(500, error);
-    });
-    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-    return res.json(premiumShows);
-  } else {
-    const premiumShows = await StitcherAPI.getPremiumShows().catch((error) => {
-      return new ApiError(500, error);
-    });
-    res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
-    return res.json(premiumShows);
+  try {
+    const { query }: { query?: string } = req.query;
+    if (query?.length) {
+      const premiumShows = await StitcherAPI.getSearchPremiumShows(query);
+      res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+      return res.json(premiumShows);
+    } else {
+      const premiumShows = await StitcherAPI.getPremiumShows();
+      res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
+      return res.json(premiumShows);
+    }
+  } catch(error) {
+    return Promise.reject(new ApiError(500, error));
   }
 };
 
