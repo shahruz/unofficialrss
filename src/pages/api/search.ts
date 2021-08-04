f9494c53e03237f8fee216068899bc9217fd992f
Troyer, Ronald <ronald.troyer@chase.com>
2021-08-04 09:23:06 -0400

    Allow for searching for all

--- src/pages/api/search.ts
+++ src/pages/api/search.ts
@@ -4,7 +4,7 @@ import { ApiError } from 'next/dist/next-server/server/api-utils';
 const api: NextApiHandler = async (req, res) => {
   try {
     const { query }: { query?: string } = req.query;
-    if (query?.length) {
+    if (query && query.trim().length) {
       const premiumShows = await StitcherAPI.getSearchPremiumShows(query);
       res.setHeader('Cache-Control', 's-maxage=3600, stale-while-revalidate');
       return res.json(premiumShows);
