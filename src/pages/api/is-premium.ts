a551af6069f81f65ca2e049bbe827f2cb23214d0
Troyer, Ronald <ronald.troyer@chase.com>
2021-09-11 13:15:16 -0400

    Use sendError

--- src/pages/api/is-premium.ts
+++ src/pages/api/is-premium.ts
@@ -1,10 +1,10 @@
 import { NextApiHandler } from 'next';
-import { ApiError, sendError } from 'next/dist/next-server/server/api-utils';
+import { sendError } from 'next/dist/next-server/server/api-utils';
 import useAuth from 'src/server/lib/useAuth';
 
 const api: NextApiHandler = async (req, res) => {
   const { token }: { token?: string } = req.cookies;
-  if (!token) return res.send(new ApiError(403, 'Missing token.'));
+  if (!token) return sendError(res, 403, 'Missing token.');
   try {
     const user = await useAuth(token);
     return res.json({ isPremium: user.isPremium, token });
